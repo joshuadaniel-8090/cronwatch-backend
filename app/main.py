4871caf2821d7952533854bbc4548d1bdcea84a1
jjoshuadaniel1234@gmail.com <jjoshuadaniel1234@gmail.com>
2026-04-26 17:14:36 +0530

    added more for cors

--- app/main.py
+++ app/main.py
@@ -19,10 +19,12 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
+        "http://localhost:8000",
         "http://localhost:5173",
         "http://127.0.0.1:3000",
         "http://127.0.0.1:5173",
         "https://aistudio.google.com",
+        "https://cronwatch.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
