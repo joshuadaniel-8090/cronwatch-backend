d0ff64317f91c57794235fae046ca6b35d4bac2c
jjoshuadaniel1234@gmail.com <jjoshuadaniel1234@gmail.com>
2026-04-26 13:44:33 +0530

    Remove __pycache__ from tracking

--- app/core/database.py
+++ app/core/database.py
@@ -1,11 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.pool import NullPool
 from app.core.config import settings
 
 engine = create_engine(
-    settings.DATABASE_URL,
-    connect_args={"sslmode": "require"},
-    pool_pre_ping=True
+    settings.DATABASE_URL + "?sslmode=require",
+    connect_args={"connect_timeout": 10},
+    pool_pre_ping=True,
+    poolclass=NullPool
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
